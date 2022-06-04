b9c7b796cdf591e0db7ca8452ca7c06a358545cd
AliMDhanani <alidhanani2000@gmail.com>
2022-06-04 16:52:12 +0100

    implemented search page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => (
                     <Link href='/' passHref>
                         <MenuItem icon={<FcHome />}>Home</MenuItem>
                     </Link>
-                    <Link href='/' passHref>
+                    <Link href='/search' passHref>
                         <MenuItem icon={<BsSearch />}>Search</MenuItem>
                     </Link>
                     <Link href='/search?purpose=for-sale' passHref>
