a1df2d22ed5182d70d13f4fdf93babfa6ece9e10
AliMDhanani <alidhanani2000@gmail.com>
2022-06-27 22:10:42 +0100

    favicon changed

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -8,7 +8,7 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-            'X-RapidAPI-Key': '08e33068famsh4f28fd60f7d6a2ep166651jsn82f1fc1e5426'
+            'X-RapidAPI-Key': '08e33068famsh4f28fd60f7d6a2ep166651jsn82f1fc1e5426',
           }
     });
 
