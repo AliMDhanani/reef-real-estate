d04d801b410e0589589318518832f7f071e593f8
AliMDhanani <alidhanani2000@gmail.com>
2022-09-05 17:05:03 +0100

    footer changed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react';
 
 const Footer = () => ( 
-    <Box textAlign="center" p="5" color="gray.100" borderTop="1px" borderColor="gray.100" backgroundColor="gray.800" paddingTop="56" paddingBottom="56">
+    <Box textAlign="center" p="5" color="gray.100" borderTop="1px" borderColor="gray.100" backgroundColor="gray.800">
         © 2021 Reef Estate, Inc.
     </Box>
 )
